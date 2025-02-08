09be8c2701a9c84d82ad384b8f94c3fdb20e1f12
Md-Alqma <mdalqma7@gmail.com>
2025-02-08 19:54:07 +0530

    created the schemas and models

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,43 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 8080;
 
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  todos: [{ type: mongoose.Schema.Types.ObjectId, ref: "Todo" }],
+});
+
+const todoSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String },
+  status: {
+    type: String,
+    enum: ["pending", "in-progress", "completed", "archived"],
+    default: "pending",
+    required: true,
+  },
+  priority: {
+    type: String,
+    enum: ["low", "medium", "high", "urgent"],
+    default: "medium",
+  },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+});
+
+const User = mongoose.model("User", userSchema);
+const Todo = mongoose.model("Todo", todoSchema);
 app.get("/", (req, res) => {
   res.send(`<h1>DooIt</h1>`);
 });
 
 try {
+  mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((err) => console.log("DB Connection Error: " + err));
   app.listen(PORT, () => {
     console.log("listening on port " + PORT);
   });
