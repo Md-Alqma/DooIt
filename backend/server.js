aceef601bf359c304039dfc67853d20123c84c45
Md-Alqma <mdalqma7@gmail.com>
2025-02-11 01:14:56 +0530

    added cors to connect frontend to backend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import todoRoutes from "./routes/todoRoutes.js";
@@ -9,6 +10,7 @@ connectDB();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/users", userRoutes);
 app.use("/api/todos", todoRoutes);
