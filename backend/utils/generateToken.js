96c5e07852e701a5f16683f8f0d4d4123a56e839
Md-Alqma <mdalqma7@gmail.com>
2025-02-15 15:56:32 +0530

    token validity decreased to 1d

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 const generateToken = (user) => {
   const payload = { username: user.username, email: user.email, _id: user._id };
-  return jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: "30d" });
+  return jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: "1h" });
 };
 
 export default generateToken;
