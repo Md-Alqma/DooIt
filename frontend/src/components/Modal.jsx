c08b338ce63d4e599d9718a9e726677022dd4394
Md-Alqma <mdalqma7@gmail.com>
2025-02-15 16:55:00 +0530

    added a modal to type the description

---
+++ frontend/src/components/Modal.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Modal = ({ open, onClose, children }) => {
+  return (
+    <div
+      onClick={onClose}
+      className={`fixed inset-0 flex justify-center items-center transition-colors ${
+        open ? "visible bg-black/20 z-10" : "invisible"
+      }`}
+    >
+      <div
+        className={`bg-white rounded-xl shadow p-6 transition-all ${
+          open ? "scale-100 opacity-100" : "scale-125 opacity-0"
+        }`}
+        onClick={(e) => e.stopPropagation()}
+      >
+        <button
+          onClick={onClose}
+          className="absolute text-lg font-black top-1 right-2 p-2 rounded-lg text-gray-600 bg-white hover:bg-gray-50 hover:text-gray-800 cursor-pointer"
+        >
+          X
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
--- frontend/src/components/TodoForm.jsx
+++ frontend/src/components/TodoForm.jsx
@@ -1,9 +1,12 @@
 import React, { useRef, useState } from "react";
 import CustomSelect from "./CutomSelect";
+import Modal from "./Modal";
 
 const TodoForm = () => {
   const [status, setStatus] = useState("pending");
   const [priority, setPriority] = useState("medium");
+  const [title, setTitle] = useState("");
+  const [open, setOpen] = useState(false);
 
   return (
     <form action="#" className="space-y-4">
@@ -13,7 +16,37 @@ const TodoForm = () => {
           type="text"
           name="title"
           placeholder="Add a new todo..."
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
+        <button
+          className="border border-[var(--color-primary)] hover:text-white hover:bg-[var(--color-primary)] rounded cursor-pointer text-gray-700  py-2 px-2"
+          type="button"
+          onClick={() => setOpen(true)}
+        >
+          Description
+        </button>
+        <Modal open={open} onClose={() => setOpen(false)}>
+          <div className="mx-auto my-4 w-full">
+            <h3 className="text-lg font-black text-gray-700 mb-2">
+              Add Description
+            </h3>
+            <textarea
+              cols="50"
+              rows="10"
+              placeholder="description..."
+              className="border border-gray-200 rounded-lg p-2 outline-gray-400"
+            />
+            <br />
+            <button
+              type="button"
+              className="border border-gray-400 w-[80px] rounded cursor-pointer bg-[var(--color-primary)] text-white p-2"
+            >
+              Add
+            </button>
+          </div>
+        </Modal>
+        {/* {toggleModal && <Modal />} */}
         <CustomSelect
           options={[
             { value: "pending" },
@@ -35,7 +68,8 @@ const TodoForm = () => {
           onChange={setPriority}
         />
         <button
-          type="submit"
+          type="button"
+          onClick={(e) => addTodo(e)}
           className="border border-gray-400 w-[80px] rounded cursor-pointer bg-[var(--color-primary)] text-white"
         >
           Add
